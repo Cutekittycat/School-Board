ef6d33844620d505cad3f66a567d81083249bc89
Cutekittycat <81061612+Cutekittycat@users.noreply.github.com>
2021-10-16 16:02:35 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 550);
@@ -14,7 +20,14 @@ function modelLoaded() {
 }
 
 function draw() {
+    
     background('#00FFFF');
+
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +" px";
+    fill('FF0000');
+     textSize(difference);
+     text('Lucky', 40, 400)
+    
 }
 
 function gotPoses(results) 
@@ -22,5 +35,12 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
